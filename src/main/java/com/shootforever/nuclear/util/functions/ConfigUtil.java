f5a3e31503dc0aed5f7094b0cc7889492653cbc3
ShootForever <2461719257@qq.com>
2024-09-22 22:11:45 +0800

    添加loadConfig（未测试）

--- src/main/java/com/shootforever/nuclear/util/functions/ConfigUtil.java
+++ src/main/java/com/shootforever/nuclear/util/functions/ConfigUtil.java
@@ -1,12 +1,15 @@
 package com.shootforever.nuclear.util.functions;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.shootforever.nuclear.Nuclear;
 import com.shootforever.nuclear.module.Module;
 import com.shootforever.nuclear.value.Value;
 import com.shootforever.nuclear.value.values.BooleanValue;
 import com.shootforever.nuclear.value.values.ChoiceValue;
 import com.shootforever.nuclear.value.values.NumberValue;
+import net.minecraft.ChatFormatting;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -27,6 +30,12 @@ public final class ConfigUtil {
         }
 
         getCurrentConfig();
+
+        if (currentConfig != null) {
+            if (!loadConfig(currentConfig)) {
+                NotifyUtil.notifyAsMessage(ChatFormatting.RED + "读取配置" + currentConfig + "失败");
+            }
+        }
     }
 
     private ConfigUtil() {
@@ -41,6 +50,52 @@ public final class ConfigUtil {
         }
     }
 
+    public static boolean loadConfig(@NotNull String name) {
+        Path configPath = CONFIG_DIR.resolve(name + ".json");
+        if (!Files.exists(configPath)) {
+            return false;
+        }
+
+        JsonObject config;
+        try {
+            config = JsonParser.parseString(String.join("\n", Files.readAllLines(configPath))).getAsJsonObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        for (String moduleName : config.keySet()) {
+            Module module = Nuclear.getInstance().getModuleManager().getModule(moduleName);
+            if (module == null) continue;
+            JsonObject moduleConfig = config.get(moduleName).getAsJsonObject();
+            for (String valueName : moduleConfig.keySet()) {
+                JsonElement newValue = moduleConfig.get(valueName);
+
+                if (valueName.equalsIgnoreCase("Enabled")) {
+                    module.setEnabled(newValue.getAsBoolean());
+                    continue;
+                }
+
+                if (valueName.equalsIgnoreCase("Bind")) {
+                    module.setKey(newValue.getAsInt());
+                    continue;
+                }
+
+                Value<?> value = module.getValue(valueName);
+                if (value == null) continue;
+                if (value instanceof NumberValue numberValue) {
+                    numberValue.setValue(newValue.getAsFloat());
+                } else if (value instanceof BooleanValue booleanValue) {
+                    booleanValue.setValue(newValue.getAsBoolean());
+                } else if (value instanceof ChoiceValue choiceValue) {
+                    choiceValue.setValue(newValue.getAsString());
+                }
+            }
+        }
+
+        return true;
+    }
+
     public static boolean saveConfig(@NotNull String name) {
         currentConfig = name;
         try {
@@ -49,25 +104,25 @@ public final class ConfigUtil {
             e.printStackTrace();
         }
 
+        JsonObject config = new JsonObject();
+        for (Module module : Nuclear.getInstance().getModuleManager().getModules()) {
+            JsonObject moduleConfig = new JsonObject();
+            moduleConfig.addProperty("Enabled", module.isEnabled());
+            moduleConfig.addProperty("Bind", module.getKey());
+            for (Value<?> value : module.getValues()) {
+                if (value instanceof NumberValue numberValue) {
+                    moduleConfig.addProperty(numberValue.getName(), numberValue.getValue());
+                } else if (value instanceof BooleanValue booleanValue) {
+                    moduleConfig.addProperty(value.getName(), booleanValue.getValue());
+                } else if (value instanceof ChoiceValue choiceValue) {
+                    moduleConfig.addProperty(value.getName(), choiceValue.getValue());
+                }
+            }
+            config.add(module.getName(), moduleConfig);
+        }
+
         Path configPath = CONFIG_DIR.resolve(name + ".json");
         try {
-            JsonObject config = new JsonObject();
-            for (Module module : Nuclear.getInstance().getModuleManager().getModules()) {
-                JsonObject moduleConfig = new JsonObject();
-                moduleConfig.addProperty("Enabled", module.isEnabled());
-                moduleConfig.addProperty("Key", module.getKey());
-                for (Value<?> value : module.getValues()) {
-                    if (value instanceof NumberValue numberValue) {
-                        moduleConfig.addProperty(numberValue.getName(), numberValue.getValue());
-                    } else if (value instanceof BooleanValue booleanValue) {
-                        moduleConfig.addProperty(value.getName(), booleanValue.getValue());
-                    } else if (value instanceof ChoiceValue choiceValue) {
-                        moduleConfig.addProperty(value.getName(), choiceValue.getValue());
-                    }
-                }
-                config.add(module.getName(), moduleConfig);
-            }
-
             Files.write(configPath, config.toString().getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
             return true;
         } catch (IOException e) {
